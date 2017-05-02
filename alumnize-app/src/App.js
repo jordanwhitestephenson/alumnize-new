473f77a5f61138b3cccb874f34bed4a61e012266
Jordan Stephenson <jordanwhitestephenson@gmail.com>
2017-05-02 15:29:33 -0600

    App finally works

--- alumnize-app/src/App.js
+++ alumnize-app/src/App.js
@@ -1,21 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {HashRouter, Route, Link, Redirect} from 'react-router-dom'
+import injectTapEventPlugin from "react-tap-event-plugin"
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+import AboutPage from './AboutPage'
+import MapPage from './MapPage'
+import LandingPage from './LandingPage'
+import ShowCompanies from './ShowCompanies'
+import FormPage from './FormPage'
+
+
+
+class App extends React.Component {
+
+    render() {
+        return (
+
+          <HashRouter>
+
+            <div>
+              <Route exact path="/" component={LandingPage}/>
+              <Route path="/AboutPage" component={AboutPage}/>
+              <Route path="/MapPage" component={MapPage}/>
+              <Route path="/ShowCompanies" component={ShowCompanies}/>
+                <Route path="/FormPage" component={FormPage}/>
+            </div>
+          </HashRouter>
+        )
+    }
 }
 
-export default App;
+export default App
---
+++ alumnize-app/src/MapPage.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class MapPage extends Component {
+    render() {
+        return (
+            <div>
+                <div className="container">
+                    <h1 className="newtitle">
+                        MAPS
+                    </h1>
+                    <p>View Where Graduates Get Hired
+                    </p>
+                    <img src = "http://www.clker.com/cliparts/y/q/h/D/v/W/orange-world-map-hi.png" alt =" YA"/>
+                </div>
+            </div>
+        )
+
+    }
+}
+
+export default MapPage;
