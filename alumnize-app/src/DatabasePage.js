86e9ea00a53d0b55206138e844c929e801236adc
Jordan Stephenson <jordanwhitestephenson@gmail.com>
2017-05-03 15:16:45 -0600

    commit before paul heroku

--- alumnize-app/src/DatabasePage.js
+++ alumnize-app/src/DatabasePage.js
@@ -1,32 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
-// import app from './server'
-// 
+import app from './server'
+
 // class Data extends Component {
 //
 // componentDidMount() {
-// // var url = '../alumnizeBack/db/MOCK_DATA.json'
-// // var that = this
-// //   fetch(url)
-// //   .then(function(response) {
-// //     if (response.status >= 400) {
-// //       throw new Error("Bad response from server");
-// //     }
-// //     return response.json();
-// //   })
-// //   .then(function(data) {
-// //     console.log(data)
-// //   });
-// // }
+// var url = '/mock'
+// var that = this
+//   fetch(url)
+//   .then(function(response) {
+//     if (response.status >= 400) {
+//       throw new Error("Bad response from server");
+//     }
+//     return response.json();
+//   })
+//   .then(function(data) {
+//     console.log(data)
+//   });
+// }
 //
 //
-// //   render() {
-// //     return (
-// //       <div className="App">
-// //
-// //       </div>
-// //     );
-// //   }
-// // }
+//   render() {
+//     return (
+//       <div className="App">
+//
+//       </div>
+//     );
+//   }
+// }
 //
 // export default Data;
--- alumnize-app/src/FormPage.js
+++ alumnize-app/src/FormPage.js
@@ -7,31 +7,32 @@ class FormPage extends Component {
     render() {
         return (
             <div>
-              <div className="back-animation back-imageCompanies back-pro content-104">
-          <div className = "damnyouReact">
-          <div className="container">
-            <div className="newrow">
-              <h1 className="newtitle">Alumnize</h1>
-              <p className = "newParagraph"> ADD YOUR GRADUATE DATA</p>
-            </div>
-          </div>
-          </div>
-          </div>
+                <div className="back-animation back-imageCompanies back-pro content-104">
+                    <div className="damnyouReact">
+                        <div className="container">
+                            <div className="newrow">
+                                <h1 className="newtitle">ADD YOUR DATA</h1>
+                                <p className="newParagraph">
+                                    Alumnize</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <div className="container">
-                  {/* <div className = "graduateData">
+                    {/* <div className = "graduateData">
                     <h1 className="newtitle">
                         {FormContainer.formPayload}
                     </h1>
                   </div> */}
-                    <img src = "http://usacdn.markmods.com/site/images/siemaIcons/256/globe%20orange.png" height= "50px" width= "50px" alt ="yaaa"/>
+                    <img src="http://usacdn.markmods.com/site/images/siemaIcons/256/globe%20orange.png" height="50px" width="50px" alt="yaaa"/>
                 </div>
                 <div className="container">
-                  <div className="columns">
-                    <div className="col-md-9 centered">
-                      <FormContainer />
+                    <div className="columns">
+                        <div className="col-md-9 centered">
+                            <FormContainer/>
 
+                        </div>
                     </div>
-                  </div>
                 </div>
             </div>
         )
